629f723e5ce7af479d8031fc7f30ac830c9143bd
suraj2617 <76090906+suraj2617@users.noreply.github.com>
2021-01-13 17:47:35 +0530

    Add files via upload

---
+++ chapter_1.py
@@ -0,0 +1,8 @@
+print("hello sparta")
+# is use for single line comment
+''' How did Elon Musk become famous?
+Elon Musk cofounded the electronic 
+payment firm PayPal and founded the
+spacecraft company SpaceX. He became 
+chief executive officer of the
+electric-car maker Tesla''' # ''' start with triple comma and ends with triple comma is a comment for multiline comments
